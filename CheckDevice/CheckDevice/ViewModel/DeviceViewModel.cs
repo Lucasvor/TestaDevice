e6b6ed46c87970c03325d729915cbcc38f917fd5
Lucas <lucasvor@github.com>
2021-12-21 14:05:37 -0300

    Add project files.

---
+++ CheckDevice/CheckDevice/ViewModel/DeviceViewModel.cs
@@ -0,0 +1,60 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace CheckDevice.ViewModel
+{
+    public class DeviceViewModel : BaseViewModel
+    {
+        public DeviceViewModel()
+        {
+            Device = DeviceInfo.Model;
+            ManuFacturer = DeviceInfo.Manufacturer;
+            DeviceName = DeviceInfo.Name;
+            DeviceType = DeviceInfo.DeviceType.ToString();
+            Platform = DeviceInfo.Platform.ToString();
+            
+        }
+
+        private string device;
+        public string Device
+        {
+            get => device;
+            set => SetProperty(ref device, value);
+        }
+
+        private string deviceType;
+        public string DeviceType
+        {
+            get => deviceType;
+            set => SetProperty(ref deviceType, value);
+        }
+
+        private string manuFacturer;
+        public string ManuFacturer
+        {
+            get => manuFacturer;
+            set => SetProperty(ref manuFacturer, value);
+        }
+
+
+        private string deviceName;
+        public string DeviceName
+        {
+            get => deviceName;
+            set => SetProperty(ref deviceName, value);
+        }
+
+        private string platform;
+        public string Platform
+        {
+            get => platform;
+            set => SetProperty(ref platform, value);
+        }
+    }
+
+    
+
+}
